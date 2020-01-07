aeddd15977336aab7e9d04447ada7b9bc8ee0cd3
Sven Axelsson <sven@axelsson.expert>
2020-01-07 16:37:19 +0100

    Reuse draw_rectangle in draw_block

--- src/draw.rs
+++ src/draw.rs
@@ -1,5 +1,5 @@
-use piston_window::{rectangle, Context, G2d};
 use piston_window::types::Color;
+use piston_window::{rectangle, Context, G2d};
 
 const BLOCK_SIZE: f64 = 25.0;
 
@@ -12,15 +12,7 @@ pub fn to_coord_u32(game_coord: i32) -> u32 {
 }
 
 pub fn draw_block(color: Color, x: i32, y: i32, con: &Context, g: &mut G2d) {
-    let gui_x = to_coord(x);
-    let gui_y = to_coord(y);
-
-    rectangle(
-        color,
-        [gui_x, gui_y, BLOCK_SIZE, BLOCK_SIZE],
-        con.transform,
-        g,
-    );
+    draw_rectangle(color, x, y, 1, 1, con, g);
 }
 
 pub fn draw_rectangle(
